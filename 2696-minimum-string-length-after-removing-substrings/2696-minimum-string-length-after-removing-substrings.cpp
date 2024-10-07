f13f7f8aae0324460f5f318c1776876d6bca647c
Vansh Aggarwal <118279381+VanshAg283@users.noreply.github.com>
2024-10-07 08:20:54 +0530

    Time: 6 ms (57.32%), Space: 10.4 MB (88.97%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minLength(string s) {
+        string stack;
+        for (char c : s) {
+            if (!stack.empty() && 
+               ((stack.back() == 'A' && c == 'B') || 
+                (stack.back() == 'C' && c == 'D'))) {
+                stack.pop_back();
+            } else {
+                stack.push_back(c);
+            }
+        }
+        return stack.size();
+    }
+};
